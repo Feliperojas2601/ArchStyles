30e3f1945c401611aded09a1df8e3cfb09949834
Felipe Rojas <juancendales@periferia-it.com>
2024-09-08 16:46:19 -0500

    Fix CORS

--- LayeredTasks/backend/src/main.ts
+++ LayeredTasks/backend/src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
